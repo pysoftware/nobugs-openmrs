bce41e7d43d4c2a74a95ac3e9f342db4d447a788
gelunata <gelunata@gmail.com>
2026-02-03 12:25:37 +0300

    Revert "Merge pull request #4 from pysoftware/datamodel"
    This reverts commit 704e98f6488257d0d768d9ce6f6d9b325158f34d, reversing
    changes made to 93172d15b405bdffc357aafac71f764946c2a7a0.
    
    Merger reversal

--- src/main/java/api/generators/annotations/GeneratingDoubleRule.java
+++
@@ -1,7 +0,0 @@
-package api.generators.annotations;
-
-public @interface GeneratingDoubleRule {
-    double min();
-    double max();
-    int precision();
-}
--- src/main/java/api/requests/skelethon/interfaces/GetAllEndpointInterface.java
+++
@@ -1,5 +0,0 @@
-package api.requests.skelethon.interfaces;
-
-public interface GetAllEndpointInterface {
-    Object getAll(Class<?> clazz);
-}
