3a9c94c659de05bd79c8991ffa2de0abb3779e92
NataliKontakt <natalikontaktgood@gmail.com>
2026-02-25 13:57:15 +0500

    add LoginPage

--- src/main/java/ui/pages/LoginPage.java
+++ src/main/java/ui/pages/LoginPage.java
@@ -6,28 +6,32 @@ import com.microsoft.playwright.Page;
 import com.microsoft.playwright.options.AriaRole;
 
 
-public class LoginPage extends BasePage{
+public class LoginPage extends BasePage {
     private final Locator INPUT_USERNAME = page.locator("input[id='username']");
     private final Locator INPUT_PASSWORD = page.locator("input[id='password']");
     private final Locator BUTTON_CONTINUE = page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions()
             .setName("Continue"));
     private final Locator BUTTON_SUBMIT = page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Log in"));
+    private final Locator WELCOME_ADMIN_TEXT = page.getByText("Welcome admin");
 
-    protected LoginPage(Page page) {
+    public LoginPage(Page page) {
         super(page);
     }
 
     @Override
-    protected String getRelativePath() {
+    public String getRelativePath() {
         return "/openmrs/spa/login";
     }
 
-    public void loginAsAdmin() {
+    public LocationPage loginAsAdmin() {
         open();
         INPUT_USERNAME.fill(Config.getProperty("admin.username"));
         BUTTON_CONTINUE.click();
         INPUT_PASSWORD.fill(Config.getProperty("admin.password"));
         BUTTON_SUBMIT.click();
+        WELCOME_ADMIN_TEXT.isVisible();
 
+        return new LocationPage(page);
     }
+
 }
