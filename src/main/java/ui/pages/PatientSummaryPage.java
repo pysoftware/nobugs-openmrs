2a4cc987b537b1be319b8bbef7be70b78e90b83f
natali-kontakt <natalikontaktgood@gmail.com>
2026-02-28 14:53:58 +0500

    add PatientSearchTest

--- src/main/java/ui/pages/PatientSummaryPage.java
+++ src/main/java/ui/pages/PatientSummaryPage.java
@@ -3,18 +3,23 @@ package ui.pages;
 import com.microsoft.playwright.Locator;
 import com.microsoft.playwright.Page;
 
+import java.util.Objects;
+
 public class PatientSummaryPage extends BasePage {
+    private String patientUuid = "";
     private final Locator TEXT_ID = page.locator("label[class*='label'][for*='identifier'] span:last-child");
     private final Locator TEXT_NAME = page.locator(".sb-avatar__text");
 
 
-    protected PatientSummaryPage(Page page) {
+    protected PatientSummaryPage(Page page, String patientUuid) {
         super(page);
+        this.patientUuid = Objects.requireNonNull(patientUuid, "patientUuid is required");
     }
+
 
     @Override
     protected String path() {
-        return "";
+        return String.format("patient/%s/chart/Patient%%20Summary", patientUuid);
     }
 
     public String getOpenMrsId() {
